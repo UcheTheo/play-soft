16931e2a4bef953a12ab1a1971c2bc13136ead26
Theophilus Uchechukwu <theo.uche2023@gmail.com>
2024-05-27 16:30:16 +0100

    Add tabs components
    - Added Songs tab with its basic layout and index

---
+++ app/(tabs)/(songs)/index.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { defaultStyles } from '@/styles'
+
+const SongsScreen = () => {
+  return (
+    <View style={defaultStyles.container}>
+      <Text style={defaultStyles.text}> SongsScreen</Text>
+    </View>
+  )
+}
+
+export default SongsScreen
+
+const styles = StyleSheet.create({})
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,18 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const TabsNavigation = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
+    <Tabs>
+      <Tabs.Screen name="favorites" />
+      <Tabs.Screen name="playlists" />
+      <Tabs.Screen name="(songs)" />
+      <Tabs.Screen name="artists" />
     </Tabs>
-  );
+  )
 }
+
+export default TabsNavigation
+
+const styles = StyleSheet.create({})
