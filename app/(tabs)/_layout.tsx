ccbf3dfc638169b00d3691b6bd1aaee4622acb68
Theophilus Uchechukwu <theo.uche2023@gmail.com>
2024-05-27 17:13:42 +0100

    Styled the tabs with blur view from expo-blur

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { colors, fontSize } from "@/constants/tokens";
+import { BlurView } from "expo-blur";
 
 const TabsNavigation = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.primary,
+        tabBarLabelStyle: {
+          fontSize: fontSize.xs,
+          fontWeight: "500",
+        },
+        headerShown: false,
+        tabBarStyle: {
+          position: "absolute",
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          borderTopWidth: 0,
+          paddingTop: 8,
+        },
+
+        tabBarBackground: () => (
+          <BlurView
+            intensity={95}
+            style={{
+              ...StyleSheet.absoluteFillObject,
+              overflow: "hidden",
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+            }}
+          />
+        ),
+      }}
+    >
       <Tabs.Screen name="favorites" />
       <Tabs.Screen name="playlists" />
       <Tabs.Screen name="(songs)" />
       <Tabs.Screen name="artists" />
     </Tabs>
-  )
-}
+  );
+};
 
-export default TabsNavigation
+export default TabsNavigation;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
